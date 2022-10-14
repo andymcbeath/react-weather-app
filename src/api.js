59a7615cdc819d4195c6cbfab7f5463ba9440e79
Andy McBeath <mcbeath1@gmail.com>
2022-10-14 16:51:45 -0500

    first commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "de54d7e969msh298af7f918a8f1ep1f3bbcjsn9dbb1b402bbf",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "";
